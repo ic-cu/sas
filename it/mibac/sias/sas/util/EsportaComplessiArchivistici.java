9eb1a6acf4f0b2906a34d60802c3d9552e8d94a9
iccu.andrea.giuliano <iccu.andrea.giuliano@gmail.com>
2014-02-19 18:39:22 +0100

    - Rimosso l'inutile metodo now(). - Riorganizzato il costruttore grazie a un paio di metodi ausiliari. - Riordinato il codice di createEntity(), isolando la definizione delle variabili dalla loro instanziazione. - Riorganizzati e migliorati i commenti, adesso alquanto più chiari. - De-staticizzato il logger.

--- it/mibac/sias/sas/util/EsportaComplessiArchivistici.java
+++ it/mibac/sias/sas/util/EsportaComplessiArchivistici.java
@@ -11,21 +11,20 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.Properties;
-
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 import org.apache.log4j.WriterAppender;
 
-import org.pmw.tinylog.*;
-
+/*
+ * Classe per la creazione degli envelope dei comlessi archivistici. A parte
+ * inizializzazioni varie, il metodo importante è creaMultiEnvelope, che ritorna
+ * un Iterator di EnvelopeWrapper. Scorrendo questo iteratore, un chiamante può
+ * decidere cosa fare di tutti gli envelope ottenuti.
+ */
 public class EsportaComplessiArchivistici
 {
 
@@ -35,8 +34,7 @@ public class EsportaComplessiArchivistici
 	public Logger log, valLog;
 	private static String logLayout = "%05r %p %C{1}.%M - %m%n";
 	String fkFonte = null;
-
-	// private static PatternLayout pl;
+	private int maxRecords;
 
 	/*
 	 * Questo costruttore prepara la connessione, impostando qualche parametro.
@@ -46,39 +44,12 @@ public class EsportaComplessiArchivistici
 	{
 		try
 		{
+			loadConfig();
+			initLogger();
 			db = new DB();
-			config = new Properties();
-			FileReader prop = new FileReader("query.prop");
-			config.load(prop);
-			prop.close();
-
-			// logger generico
-			log = Logger.getLogger("COMPARC");
-			log.setLevel(Level.INFO);
-			PatternLayout pl = new PatternLayout(logLayout);
-			File lf = new File("log");
-			PrintWriter pw = new PrintWriter(lf);
-			WriterAppender wa = new WriterAppender(pl, pw);
-			log.addAppender(wa);
-			wa = new WriterAppender(pl, System.out);
-			log.addAppender(wa);
-			// BasicConfigurator.configure(wa);
-
-			// serve un logger per la validazione
-			valLog = Logger.getLogger("VALIDATE");
-			pl = new PatternLayout("%m%n");
-			lf = new File("validate.log");
-			pw = new PrintWriter(lf);
-			wa = new WriterAppender(pl, pw);
-			valLog.addAppender(wa);
-			wa = new WriterAppender(pl, System.out);
-			valLog.addAppender(wa);
-			// BasicConfigurator.configure(wa);
-
 			connection = db.getConnection();
 			log.info("Inizio esportazione complessi archivistici");
 			org.pmw.tinylog.Logger.info("Inizio esportazione complessi archivistici");
-			fkFonte = config.getProperty("sogc.fk_fonte");
 		}
 		catch(FileNotFoundException e)
 		{
@@ -90,57 +61,92 @@ public class EsportaComplessiArchivistici
 		}
 	}
 
-	public static XMLGregorianCalendar now()
-	{
-		// È frequente l'uso di XMLGregorianCalendar, così macchinoso da rendere
-		// sicuramente auspicabile la creazione di metodi ausiliari opportuni
+	// carica la configurazione sotto forma di Properties
 
-		GregorianCalendar gc = new GregorianCalendar();
-		XMLGregorianCalendar xgc = null;
-		try
-		{
-			xgc = DatatypeFactory.newInstance().newXMLGregorianCalendar(gc);
-		}
-		catch(DatatypeConfigurationException e)
-		{
-			e.printStackTrace();
-		}
-		return xgc;
+	private void loadConfig() throws FileNotFoundException, IOException
+	{
+		config = new Properties();
+		FileReader prop = new FileReader("query.prop");
+		config.load(prop);
+		prop.close();
+		maxRecords = Integer.parseInt(config.getProperty("xml.output.maxrecords"));
+		fkFonte = config.getProperty("sogc.fk_fonte");
 	}
 
-	/*
-	 * Crea una lista di envelope, uno per ogni soggetto conservatore. Ritorna un
-	 * iterator per comodità
-	 */
+	// inizializza il logger
+
+	private void initLogger() throws FileNotFoundException
+	{
+		// logger generico
+		log = Logger.getLogger("COMPARC");
+		log.setLevel(Level.INFO);
+		PatternLayout pl = new PatternLayout(logLayout);
+		File lf = new File("log");
+		PrintWriter pw = new PrintWriter(lf);
+		WriterAppender wa = new WriterAppender(pl, pw);
+		log.addAppender(wa);
+		wa = new WriterAppender(pl, System.out);
+		log.addAppender(wa);
+		// BasicConfigurator.configure(wa);
+
+		// serve un logger per la validazione
+		valLog = Logger.getLogger("VALIDATE");
+		pl = new PatternLayout("%m%n");
+		lf = new File("validate.log");
+		pw = new PrintWriter(lf);
+		wa = new WriterAppender(pl, pw);
+		valLog.addAppender(wa);
+		wa = new WriterAppender(pl, System.out);
+		valLog.addAppender(wa);
+		// BasicConfigurator.configure(wa);
+	}
+
+/*
+ * Crea una lista di envelope, ognuno contenente al massimo maxRecords entity
+ * dcomparc. Ritorna un iterator per comodità.
+ */
+
 	public Iterator<EnvelopeWrapper> creaMultiEnvelope(int idIstituto)
 	{
-		RecordList rl = null;
 		it.beniculturali.sas.catalogo.envelope_catsas.ObjectFactory envObf;
 		envObf = new it.beniculturali.sas.catalogo.envelope_catsas.ObjectFactory();
-		String codiProvenienzaDComparc = null;
+		RecordList rl = null;
 		EnvelopeWrapper ew = null;
 		ArrayList<EnvelopeWrapper> ewl = null;
-		int maxRecords = 100;
-
-		ewl = new ArrayList<EnvelopeWrapper>();
-		/*
-		 * Si crea una entity in cui metteremo un sogc. Più avanti la entity sarà
-		 * aggiunta alla recordlist
-		 */
-		Entity ent = null;
-		ComplessiArchivistici ca;
-		ca = new ComplessiArchivistici(idIstituto, db.getConnection());
-		Iterator<DComparc> di = ca.createEntity(idIstituto);
 		DComparc dcomparc = null;
+		Entity ent = null;
+		ComplessiArchivistici ca = null;
+		Iterator<DComparc> di = null;
+		RecordWrapper rw = null;
 		int i = 0;
+
+/*
+ * Si istanzia la lista di envelope ewl e la lista dei complessi di, passando
+ * per la classe ComplessiArchivistici. Il metodo createEntity ha un nome
+ * improprio, perché in realtà ritorna una lista, e andrebbe cambiato.
+ */
+		ewl = new ArrayList<EnvelopeWrapper>();
+		ca = new ComplessiArchivistici(idIstituto, db.getConnection());
+		di = ca.createEntity(idIstituto);
+
+/*
+ * Si cicla sui dcomparc ottenuti, tutti quelli dell'istituto attuale
+ */
+
 		while(di.hasNext())
 		{
-			/*
-			 * si cicla sugli id trovati, creando un envelope per ciascuno
-			 */
 			dcomparc = di.next();
 			System.err.println("complesso numero " + i);
-			codiProvenienzaDComparc = dcomparc.getCodiProvenienza();
+
+/*
+ * Se il contatore è un multiplo del massimo numero di record per envelope, si
+ * crea un nuovo envelope, anche se nascosto dentro un wrapper. All'enveloper va
+ * aggiunta una nuova recordlist, e poi si può aggiungere l'envelope alla lista
+ * degli envelope. I prossimi maxRecords dcomparc saranno aggiunti a questa
+ * recordlist, poi se ne farà una nuova. Il test è positivo anche alla prima
+ * iterazione, quando il contatore è nullo.
+ */
+
 			if(i++ % maxRecords == 0)
 			{
 				ew = new EnvelopeWrapper();
@@ -150,26 +156,32 @@ public class EsportaComplessiArchivistici
 				ew.setRecordList(rl);
 				ewl.add(ew);
 			}
-			/*
-			 * Per evitare errori di validazione delle fonti, non tutte codificate
-			 * correttamente negli XSD, si usa una fonte fittizia sicuramente valida
-			 */
+
+/*
+ * Arrivati qui, una recordlist è pronta, nuova o già parzialmente popolata.
+ * Possiamo aggiungere record. Per evitare errori di validazione delle fonti,
+ * non tutte codificate correttamente negli XSD, si usa una fonte fittizia
+ * sicuramente valida, inclusa nella configurazione, ma solo se questa è non
+ * nulla, altrimenti si usa quella risultante dal dcomparc attuale
+ */
+
 			if(fkFonte != null)
 			{
 				ew.setFonte(fkFonte);
 			}
 			else
 			{
-				// ew.setFonte(codiProvenienzaDComparc);
 				ew.setFonte(dcomparc.getFkFonte().getProfGroup().getGroupName());
 			}
 
-			// Si crea una entity a cui si aggiunge il comparc e che poi si aggiunge
-			// al recordbody più avanti
-			ent = envObf.createEnvelopeRecordListRecordRecordBodyEntity();
-			ent.getContent().add(dcomparc); // ancona
+/*
+ * Si inserisce il dcomparc attuale in una nuova entity, poi questa ad un nuovo
+ * record, e il nuovo record alla recordlist dell'envelope attuale
+ */
 
-			RecordWrapper rw = new RecordWrapper();
+			ent = envObf.createEnvelopeRecordListRecordRecordBodyEntity();
+			ent.getContent().add(dcomparc);
+			rw = new RecordWrapper();
 			rw.setDIRECTIVE("UPSERT");
 			rw.setRecordIdentifier("CA-" + dcomparc.getCodiProvenienza());
 			rw.setRecordDatestamp();
