e2e8166bafb1454c1b685b6e060931fe785175d6
sarkiaponius <sarkiaponius@alice.it>
2014-09-08 22:30:04 +0200

    - Chiuso il BufferedInputStream subito dopo il ciclo che lo usa (c'era un warning che consigliava la chiusura esplicita). - Riformattato il codice con le graffe ciascuna da sola su una riga, e con i tab sostituiti da due spazi.

--- src/it/mibac/sias/sas/util/Spedizione.java
+++ src/it/mibac/sias/sas/util/Spedizione.java
@@ -9,69 +9,79 @@ import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-public class Spedizione {
-	private String percorso; // percorso della directory da comprimere
-	private String fileZip; // nome completo del file zip da produrre
+public class Spedizione
+{
+  private String percorso; // percorso della directory da comprimere
+  private String fileZip; // nome completo del file zip da produrre
 
-	public String getPercorso() {
-		return percorso;
-	}
+  public String getPercorso()
+  {
+    return percorso;
+  }
 
-	public void setPercorso(String percorso) {
-		this.percorso = percorso;
-	}
+  public void setPercorso(String percorso)
+  {
+    this.percorso = percorso;
+  }
 
-	public String getFileZip() {
-		return fileZip;
-	}
+  public String getFileZip()
+  {
+    return fileZip;
+  }
 
-	public void setFileZip(String fileZip) {
-		this.fileZip = fileZip;
-	}
+  public void setFileZip(String fileZip)
+  {
+    this.fileZip = fileZip;
+  }
 
-	public Spedizione(String percorso, String fileZip) {
-		super();
-		setPercorso(percorso);
-		setFileZip(fileZip);
-	}
+  public Spedizione(String percorso, String fileZip)
+  {
+    super();
+    setPercorso(percorso);
+    setFileZip(fileZip);
+  }
 
-	public void comprimi() {
-		try {
-			FileOutputStream fos = new FileOutputStream(getFileZip());
-			ZipOutputStream zos = new ZipOutputStream(fos);
-			ZipEntry ze;
+  public void comprimi()
+  {
+    try
+    {
+      FileOutputStream fos = new FileOutputStream(getFileZip());
+      ZipOutputStream zos = new ZipOutputStream(fos);
+      ZipEntry ze;
 
-			File dir = new File(getPercorso());
-			if (dir.isDirectory()) {
-				File[] lista = dir.listFiles();
-				for (int i = 0; i < lista.length; i++) {
-					File entry = lista[i];
-					ze = new ZipEntry(entry.getName());
-					FileInputStream fis; // dichiaraxione
-					fis = new FileInputStream(entry); // istanziazione
-					byte[] buffer = new byte[2048];
-					BufferedInputStream bis = new BufferedInputStream(fis, 2048);
-					zos.putNextEntry(ze);
-					int count;
-					while ((count = bis.read(buffer, 0, 2048)) != -1) {
-						zos.write(buffer, 0, count);
-
-					}
-					zos.flush();
-					zos.closeEntry();
-				}
-				zos.flush();
-				zos.close();
-
-			}
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
+      File dir = new File(getPercorso());
+      if(dir.isDirectory())
+      {
+        File[] lista = dir.listFiles();
+        for(int i = 0; i < lista.length; i++)
+        {
+          File entry = lista[i];
+          ze = new ZipEntry(entry.getName());
+          FileInputStream fis; // dichiarazione
+          fis = new FileInputStream(entry); // istanziazione
+          byte[] buffer = new byte[2048];
+          BufferedInputStream bis = new BufferedInputStream(fis, 2048);
+          zos.putNextEntry(ze);
+          int count;
+          while((count = bis.read(buffer, 0, 2048)) != -1)
+          {
+            zos.write(buffer, 0, count);
+          }
+          bis.close();
+          zos.flush();
+          zos.closeEntry();
+        }
+        zos.flush();
+        zos.close();
+      }
+    }
+    catch(FileNotFoundException e)
+    {
+      e.printStackTrace();
+    }
+    catch(IOException e)
+    {
+      e.printStackTrace();
+    }
+  }
 }
