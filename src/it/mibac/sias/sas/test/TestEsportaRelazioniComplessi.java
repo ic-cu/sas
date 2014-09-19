fc2adc8d4fbdbc11c8a25cee1e2a14f1c036b131
iccu.andrea.giuliano <iccu.andrea.giuliano@gmail.com>
2014-09-19 15:35:25 +0200

    - Vari ritocchi all'esportazione delle relazioni soggetti-complessi, fra cui migliorie alla gestione delle stringhe data, contatore, percorsi file vari.

--- src/it/mibac/sias/sas/test/TestEsportaRelazioniComplessi.java
+++ src/it/mibac/sias/sas/test/TestEsportaRelazioniComplessi.java
@@ -36,6 +36,12 @@ public class TestEsportaRelazioniComplessi
 	{
 		Properties config = new Properties();
 		Properties fontiMap = new Properties();
+
+/*
+ * Questa stringa serve come separatore delle varie componenti dei nomi dei file ZML e ZIP, e
+ * andrebbe esternalizzata.
+ */
+
 		String sep = "_";
 		log = Logger.getLogger("COMPARC");
 		try
@@ -61,17 +67,15 @@ public class TestEsportaRelazioniComplessi
 		PrintWriter pw = null;
 		File tDir = null;
 
-		SimpleDateFormat sdf;
-		sdf = new SimpleDateFormat("yyyyMMdd");
-		String today = sdf.format(new Date());
-		String fileName;
-		DecimalFormat df;
-		df = new DecimalFormat("000");
+/*
+ * Si ricava la data di oggi nel formato giusto. Questo formato andrebbe esternalizzato in un file
+ * di Properties.
+ */
+
+		String today = new SimpleDateFormat("yyyyMMdd").format(new Date());
 
 		// si creano le directory temporanee, caso mai non esistessero
 
-		tDir = new File(tmpDir + "/rc/xml/" + today);
-		tDir.mkdirs();
 		tDir = new File(tmpDir + "/rc/zip/" + today);
 		tDir.mkdirs();
 
@@ -85,21 +89,34 @@ public class TestEsportaRelazioniComplessi
 			e1.printStackTrace();
 		}
 		ResultSet rs;
-		int maxIstituti = 1000;
 		EsportaRelazioniComplessi erc = new EsportaRelazioniComplessi();
 		try
 		{
 			stmtIstituti.execute();
 			rs = stmtIstituti.getResultSet();
 
+/*
+ * Serve un formato per il numero progressivo da aggiungere ad ogni file XML prima dell'estensione.
+ * Anche questo formato dovrebbe andare in un file di Properties
+ */
+
+			DecimalFormat df;
+			df = new DecimalFormat("000");
+
+/*
+ * Si comincia a iterare su tutti gli istituti. C'è un valore di taglio a solo scopo di debugging.
+ */
+
+			int maxIstituti = 1000;
 			while(rs.next() && (maxIstituti-- > 0))
 			{
 				idIstituto = rs.getInt("ID_Istituto");
 
 // La fonte è ricavata dalla query, ma tramite una mappa che funziona solo per fonti senza "-".
 // Altrove è invece estratta con il "-" e questo comportamento andrebbe uniformato.
-				
+
 				fonte = fontiMap.getProperty(rs.getString("fk_fonte"));
+
 // idIstituto = 794000000; fonte = "ITASRM";
 // idIstituto = 228200000; fonte = "ITASBO";
 // idIstituto = 180900000; fonte = "ITASBL";
@@ -108,37 +125,59 @@ public class TestEsportaRelazioniComplessi
 //				idIstituto = 480800000;
 //				fonte = fontiMap.getProperty("ITASAQ");
 // idIstituto = 450180000; fonte = "ITASIM";
-				
-				Iterator<EnvelopeWrapper> ewi = erc.creaMultiEnvelope(idIstituto, fonte);
-				EnvelopeWrapper ew = null;
-				int i = 0;
 
-// si crea il necessario alla gestione del file ZIP
+/*
+ * Si ricava l'iteratore sulle relazioni di questo istituto con i suoi complessi e fra i complessi
+ * stessi. Serve anche un contatore per numerare i diversi file XML della stessa fonte.
+ */
 
-				String zipFileName = tmpDir + "/rc/zip/" + today + "/SIAS" + sep + fonte + sep + sdf.format(new Date()) + ".zip";
-				String percorsoXML = tmpDir + "/rc/xml/" + today;
-				while(ewi.hasNext())
+				if(fonte != null && !fonte.equals(""))
 				{
-					ew = ewi.next();
-					// fileName = "SIAS-ITASVT-";
-					fileName = ew.getSource() + sep;
-					fileName += fonte + sep;
-// fileName += ew.getFonte() + sep;
-					fileName += sdf.format(new Date()) + sep;
-					fileName += df.format(++i);
-					fileName += ".xml";
+					log.info("Istituto " + idIstituto + "(" + fonte + "), preparazione degli envelope relazioni complessi");
+					Iterator<EnvelopeWrapper> ewi = erc.creaMultiEnvelope(idIstituto, fonte);
+					EnvelopeWrapper ew = null;
+					int i = 0;
+
+/*
+ * Si crea il necessario alla gestione del file ZIP. Tutti i file ZIP di oggi vanno nella stessa
+ * directory, ma ovviamente hanno nomi diversi a seconda della fonte e della data.
+ */
+					String zipFileName = tmpDir + "/rc/zip/" + today + "/SIAS" + sep + fonte + sep + today + ".zip";
+
+/*
+ * I percorsi dei file XML di oggi vanno distinti in base alla fonte
+ */
+
+					String percorsoXML = tmpDir + "/rc/xml/" + today + "/" + fonte;
+					tDir = new File(percorsoXML);
+					tDir.mkdirs();
+
+					while(ewi.hasNext())
+					{
+						ew = ewi.next();
+						String fileName;
+						fileName = ew.getSource() + sep;
+						fileName += fonte + sep;
+						fileName += today + sep;
+						fileName += df.format(++i);
+						fileName += ".xml";
 
 // col marshall viene effettivamente creato un file XML (a meno di flush)
 
-					log.info("Istituto " + fonte + ", envelope numero " + i);
-					pw = new PrintWriter(new File(percorsoXML + "/" + fileName));
-					ew.marshall(pw);
-				}
+						log.info("Istituto " + fonte + ", envelope numero " + i);
+						pw = new PrintWriter(new File(percorsoXML + "/" + fileName));
+						ew.marshall(pw);
+					}
 
 // si comprimono tutti gli envelope in un unico file zip
 
-				Spedizione sped = new Spedizione(percorsoXML, zipFileName);
-				sped.comprimi();
+					Spedizione sped = new Spedizione(percorsoXML, zipFileName);
+					sped.comprimi();
+				}
+				else
+				{
+					log.warn("Istituto " + idIstituto + " con fonte \'" + fonte + "\' scartato");
+				}
 			}
 		}
 		catch(SQLException e)
@@ -149,7 +188,5 @@ public class TestEsportaRelazioniComplessi
 		{
 			e.printStackTrace();
 		}
-
-		// esc.creaEnvelope().marshall();
 	}
 }
